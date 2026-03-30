b931d3a4b50974dd05182d7f17c9efc450e8d952
Top <Top@WIN-E2BGOJJHORA>
2026-03-30 11:22:21 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Circle : Figure
+    {
+        public Double Radius { get; set; }
+        public Circle(string color, string name, double radius)
+        {
+            Color = color;
+            Name = name;
+            Radius = radius;
+        }
+        public override double GetArea()
+        {
+            return 3.14 * Radius * Radius;
+        }
+        public override double GetPerimeter()
+        {
+            return 2 * 3.14 * Radius;
+        }
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, Радиус - {Radius}, Площадь - {GetArea()}, Периметр - {GetPerimeter()}";
+        }
+    }
+}
---
+++ ConsoleApp1/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Rectangle : Figure
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(string color, string name, double width, double height)
+        {
+            Color = color;
+            Name = name;
+            Width = width;
+            Height = height;
+        }
+        public override double GetArea()
+        {
+            return (Width * Height);
+        }
+        public override double GetPerimeter()
+        {
+            return 2 * (Width * Height);
+        }
+        public override string GetInfo()
+        {
+            return $"{base.GetInfo()}, Ширина - {Width}, Высота - {Height}, Площадь - {GetArea()}, Периметр - {GetPerimeter()},";
+        }
+    }
+}
